dfc2ef3a7719880f253d375c4a9bfd1fcf051f4a
danelian <141317269+danelian@users.noreply.github.com>
2023-11-11 23:28:26 +0300

    acf: block main

--- front-page.php
+++ front-page.php
@@ -3,26 +3,52 @@
 <!-- MAIN -->
 <section class="main">
   <div class="main__cards">
-    <a href="#" class="main-card one" style="background-image: url('./img/izgotovlenie.jpg')">
-      <h1>Изготавливление сложных корпусных изделий</h1>
+
+    <a href="<?php the_field('main-one_link'); ?>" class="main-card one" style="background-image: url('<?php the_field('main-one_image'); ?>')">
+      <?php if (get_field('main-one_title')) { ?>
+        <h1><?php the_field('main-one_title'); ?></h1>
+      <?php } ?>
+      <?php if (get_field('main-one_text')) { ?>
+        <p><?php the_field('main-one_text'); ?></p>
+      <?php } ?>
     </a>
-    <a href="#" class="main-card two" style="background-image: url('./img/gibka.jpg')">
-      <h1>Гибка листового металла</h1>
-      <p>Формирование, угловые изгибы, кромка, прочность, кастомизация.</p>
+
+    <a href="<?php the_field('main-two_link'); ?>" class="main-card two" style="background-image: url('<?php the_field('main-two_image'); ?>')">
+      <?php if (get_field('main-two_title')) { ?>
+        <h1><?php the_field('main-two_title'); ?></h1>
+      <?php } ?>
+      <?php if (get_field('main-two_text')) { ?>
+        <p><?php the_field('main-two_text'); ?></p>
+      <?php } ?>
     </a>
-    <a href="#" class="main-card three" style="background-image: url('./img/pokraska.jpg')">
-      <h1>Покраска</h1>
-      <p>Формирование, угловые изгибы, кромка, прочность, кастомизация.</p>
+
+    <a href="<?php the_field('main-three_link'); ?>" class="main-card three" style="background-image: url('<?php the_field('main-three_image'); ?>')">
+      <?php if (get_field('main-three_title')) { ?>
+        <h1><?php the_field('main-three_title'); ?></h1>
+      <?php } ?>
+      <?php if (get_field('main-three_text')) { ?>
+        <p><?php the_field('main-three_text'); ?></p>
+      <?php } ?>
     </a>
-    <a href="#" class="main-card four" style="background-image: url('./img/svarka.jpg')">
-      <h1>Сварка</h1>
-      <p>Соединение, прочность, точность, автоматизация, качество, многослойность, инновации.</p>
+
+    <a href="<?php the_field('main-four_link'); ?>" class="main-card four" style="background-image: url('<?php the_field('main-four_image'); ?>')">
+      <?php if (get_field('main-four_title')) { ?>
+        <h1><?php the_field('main-four_title'); ?></h1>
+      <?php } ?>
+      <?php if (get_field('main-four_text')) { ?>
+        <p><?php the_field('main-four_text'); ?></p>
+      <?php } ?>
     </a>
-    <a href="#" class="main-card five" style="background-image: url('./img/rezka.jpg')">
-      <h1>Резка листового металла</h1>
-      <p>Точность, скорость, надежность, инновации, прецизия, эффективность, многоразовость, автоматизация, качество,
-        многообразие.</p>
+
+    <a href="<?php the_field('main-five_link'); ?>" class="main-card five" style="background-image: url('<?php the_field('main-five_image'); ?>')">
+      <?php if (get_field('main-five_title')) { ?>
+        <h1><?php the_field('main-five_title'); ?></h1>
+      <?php } ?>
+      <?php if (get_field('main-five_text')) { ?>
+        <p><?php the_field('main-five_text'); ?></p>
+      <?php } ?>
     </a>
+
   </div>
 </section>
 
